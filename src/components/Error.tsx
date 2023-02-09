02325d681ac543e9f98e3b4ab9b7baa7b50aaf27
Nemanja Malesija <malesija13@gmail.com>
2023-02-09 16:30:11 +0100

    Complete Error page

--- src/components/Error.tsx
+++ src/components/Error.tsx
@@ -1,5 +1,12 @@
 const Error = () => {
-  return <div>Error</div>;
+  return (
+    <div className="section-error">
+      <h1 className="main-heading heading-error">Error 404</h1>
+      <p className="error-paragraph">
+        The page you are looking for does not exist on this website
+      </p>
+    </div>
+  );
 };
 
 export default Error;
